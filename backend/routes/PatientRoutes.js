042b99a8ee194dd5a9711d9bbd5a561f70332a70
afnannaseem <139938335+afnannaseem@users.noreply.github.com>
2024-11-10 15:58:24 +0500

    Delete backend/routes directory

--- backend/routes/PatientRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPatientData } = require("../controllers/PatientController");
-
-router.get("/patient", getPatientData);
-
-module.exports = router;
