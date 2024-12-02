3c6c4adf04c4035d5b580a59f827d2db38b09abf
afnannaseem <afnannaseem837@gmail.com>
2024-12-03 01:30:59 +0500

    Fixed Merge Conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,18 @@ import SignupPage from "./pages/Singup";
 import LoginPage from "./pages/Login";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
-<<<<<<< HEAD
 
-
-=======
 import MonitoringDashboard from "./pages/MonitoringDashboard";
 
->>>>>>> e3f7855bf26bd7242a7ed15c9c61fc939bd0a87c
 const App = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
-        
-        <Route path="/" element={<SignupPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        
-        <Route
-          path="/profile"
-          element={<ProtectedRoute element={Profile} />}
-        />
+        <Route path="/" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-=======
+        <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
+
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -34,7 +24,6 @@ const App = () => {
           path="/dashboard"
           element={<ProtectedRoute element={MonitoringDashboard} />}
         />
->>>>>>> e3f7855bf26bd7242a7ed15c9c61fc939bd0a87c
       </Routes>
     </Router>
   );
